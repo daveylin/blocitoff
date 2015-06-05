ce8c1087fd2c2d288eb5d181db57831e32d69cc9
David <daveylin@gmail.com>
2015-06-05 15:45:31 +0000

    adding todo items has error name filed is empty

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,18 @@
 class ItemsController < ApplicationController
   def create
+    @item = Item.new( item_params )
+    @user = current_user
+
+    if @item.save
+      redirect_to [@user], notice: "Item was saved successfully."
+    else
+      flash[:error] = "There was an error saving the item. Please try again."
+    end
+  end
+  
+  private
+
+  def item_params
+    params.require(:name)
   end
 end
